d7ca60b7f1c6fedb88e96a04817afc718de02b1f
mohitSanwal <mohit123ongraph@gmail.com>
2023-08-27 14:21:29 +0530

    footer added

---
+++ ui-react/src/Footer.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+
+export default function Footer() {
+    return(
+        <>
+           <div className="footer">
+                <span className="centerTxt"> footer content </span>
+           </div>
+        </>
+    )
+}
