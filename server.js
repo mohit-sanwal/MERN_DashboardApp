eefc32e285df41907294230d42543687e51b2dbc
mohitSanwal <mohit123ongraph@gmail.com>
2023-08-29 05:14:46 +0530

    signup/register api implementation

--- server.js
+++ server.js
@@ -1,38 +1,34 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const cors=require("cors");
-
+const cors = require("cors");
+require('./db/config');
+const User = require("./db/model/User");
 const app = express();
-
-const connectDB = async() => {
-  mongoose.connect('mongodb://localhost:27017/e-comm');
-  const productSchema = new mongoose.Schema({});
-  const product = mongoose.model('product', productSchema);
-  const data = await product.find();
-  console.log(data);
-}
-
-connectDB()
-
-const corsOptions ={
-   origin:'*', 
-   credentials:true, //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
-
-app.use(cors(corsOptions));
 app.use(express.json());
 
+const corsOptions ={
+  origin:'*', 
+  credentials:true, //access-control-allow-credentials:true
+  optionSuccessStatus:200,
+}
 
-app.get('/app', (req, resp) => {
+app.post('/register', async (req, resp) => {
   // console.log('test', req, resp);
-  resp.send({data: 'app is working---'})
+  let user = new User(req.body);
+  console.log('user model', user);
+  let result = await user.save();
+  console.log('user model', user);
+  resp.send(result)
 })
-
 
 app.listen(5000);
 
 
+// const connectDB = async() => {
+//   mongoose.connect('mongodb://localhost:27017/e-comm');
+//   const productSchema = new mongoose.Schema({});
+//   const product = mongoose.model('product', productSchema);
+//   const data = await product.find();
+//   console.log(data);
+// }
 
-
-
+// connectDB()
