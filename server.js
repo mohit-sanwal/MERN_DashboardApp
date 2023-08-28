062a921cf7eb199795044c89ec37a1fd2c1e83ae
mohitSanwal <mohit123ongraph@gmail.com>
2023-08-28 17:21:09 +0530

    creating a connection between mongo and mode via mongoose

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const cors=require("cors");
 
 const app = express();
+
+const connectDB = async() => {
+  mongoose.connect('mongodb://localhost:27017/e-comm');
+  const productSchema = new mongoose.Schema({});
+  const product = mongoose.model('product', productSchema);
+  const data = await product.find();
+  console.log(data);
+}
+
+connectDB()
 
 const corsOptions ={
    origin:'*', 
